eaed792db6d3643c97ce067afbc8f4d49f2e1467
Elimali hamza <elimal.hamza@gmail.com>
2024-01-13 10:53:23 -0800

    .

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -17,33 +17,23 @@ def do_pack():
     local(f"tar -cvzf {archive_path} web_static")
     return archive_path
 
-
 def do_deploy(archive_path):
-    """Distribute an archive to web servers."""
+    """ deploy """
+    if (os.path.isfile(archive_path) is False):
+        return False
+
     try:
-        if not path.exists(archive_path):
-            return False
-
+        new_comp = archive_path.split("/")[-1]
+        new_folder = ("/data/web_static/releases/" + new_comp.split(".")[0])
         put(archive_path, "/tmp/")
-
-        filename = archive_path.split("/")[-1]
-        folder_name = f"/data/web_static/releases/{filename.split('.')[0]}"
-        run("mkdir -p {}".format(folder_name))
-        run("tar -xzf /tmp/{} -C {}".format(filename, folder_name))
-
-        run("rm /tmp/{}".format(filename))
-
-        run("mv {}/web_static/* {}".format(folder_name, folder_name))
-
-        run("rm -rf {}/web_static".format(folder_name))
-
-        run("rm -rf /data/web_static/current")
-
-        run("ln -s {} /data/web_static/current".format(folder_name))
-
-        print("New version deployed!")
+        run("sudo mkdir -p {}".format(new_folder))
+        run("sudo tar -xzf /tmp/{} -C {}".
+            format(new_comp, new_folder))
+        run("sudo rm /tmp/{}".format(new_comp))
+        run("sudo mv {}/web_static/* {}/".format(new_folder, new_folder))
+        run("sudo rm -rf {}/web_static".format(new_folder))
+        run('sudo rm -rf /data/web_static/current')
+        run("sudo ln -s {} /data/web_static/current".format(new_folder))
         return True
-
-    except Exception as err:
-        print(err)
+    except:
         return False
