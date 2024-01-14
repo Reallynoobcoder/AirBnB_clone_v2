c8b5b58c2c6e259bd38b90c543413d058e28fc10
Elimali hamza <elimal.hamza@gmail.com>
2024-01-14 10:43:20 -0800

    .

---
+++ 100-clean_web_static.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""
+Fabric script (based on the file 3-deploy_web_static.py) that deletes
+out-of-date archives using the function do_clean.
+"""
+
+from fabric.colors import red
+from fabric.api import local, run, env, cd, lcd
+
+env.hosts = ['54.236.231.98', '54.197.46.38']
+
+
+def do_clean(number=0):
+    """Delete unnecessary archives on web servers."""
+    number = int(number)
+    if number < 0:
+        print(red("Number must be non-negative."))
+        return
+
+    if number == 0:
+        return
+
+    with lcd("versions"):
+        local_archives = local("ls -1t", capture=True).splitlines()
+        local_archives_to_keep = local_archives[:number]
+        local_archives_to_remove = local_archives[number:]
+        for archive in local_archives_to_remove:
+            local(f"rm -f {archive}")
+
+    with cd("/data/web_static/releases"):
+        remote_archives = run("ls -1t").splitlines()
+        remote_archives_to_keep = remote_archives[:number]
+        remote_archives_to_remove = remote_archives[number:]
+        for archive in remote_archives_to_remove:
+            run(f"rm -rf {archive}")
