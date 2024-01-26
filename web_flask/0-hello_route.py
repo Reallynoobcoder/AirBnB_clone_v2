023ab123fa8015420c3c247894e20cbbede1a500
El imali hamza <elimal.hamza@gmail.com>
2024-01-26 15:24:41 +0000

    .

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -2,13 +2,13 @@
 """Flask web application"""
 
 from flask import Flask
-
 app = Flask(__name__)
+
 
 @app.route("/", strict_slashes=False)
 def hello():
     """Return hello"""
-    return "“Hello HBNB!”"
+    return "Hello HBNB!"
 
 
 if __name__ == "__main__":
