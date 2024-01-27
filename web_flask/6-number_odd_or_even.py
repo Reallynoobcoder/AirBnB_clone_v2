64ea83cb3e30a4092b83e07306aa2d505f7df174
El imali hamza <elimal.hamza@gmail.com>
2024-01-27 14:22:33 +0000

    flask 6 templates odd/even

---
+++ web_flask/6-number_odd_or_even.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python3
+"""Flask web application"""
+from flask import Flask, render_template
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello():
+    """Return hello"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """Return HBNB when /hbnb URL is accessed"""
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def c_fun(text):
+    """Return C followed by input variable"""
+    text_spaces = text.replace('_', ' ')
+    return f"C {text_spaces}"
+
+
+@app.route("/python", strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def python_cool(text="is cool"):
+    """Return Python with optional text"""
+    text_spaces = text.replace('_', ' ')
+    return f"Python {text_spaces}"
+
+
+@app.route("/number/<int:n>", strict_slashes=False)
+def number_n(n):
+    """Return n if is an integer"""
+    return f"{n} in a number"
+
+
+@app.route("/number_template/<int:n>", strict_slashes=False)
+def html_template(n):
+    """Render html template if n is int"""
+    return render_template("5-number.html", number=n)
+
+
+@app.route("/number_odd_or_even/<int:n>", strict_slashes=False)
+def odd_even(n):
+    """Render html template if is an int, indicating if it's even or odd"""
+    if n % 2 == 0:
+        num = "even"
+    else:
+        num = "odd"
+    return render_template("5-number.html", number=n, num=num)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
